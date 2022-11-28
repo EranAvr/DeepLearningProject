7abce5848959788984bda4dc341c5bd2113d20ee
Eran Avraham <48490120+EranAvr@users.noreply.github.com>
2022-11-28 10:10:00 +0200

    Update citrus_diseases.py

--- citrus_diseases.py
+++ citrus_diseases.py
@@ -1,5 +1,7 @@
 import numpy as np  # linear algebra
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+import pandas as pd
 import os
 import torch
 import torch.optim as optim
@@ -67,13 +69,13 @@ class ImageClassificationModel(nn.Module):
 """
 
 
-def train(model, optimizer):
+def train(model, optimizer, data_loader: DataLoader):
     model.train()
     for epoch in range(EPOCHS):
         print(f'>>> Epoch #{epoch}')
         batch_count = 0
         img_checked = 0
-        for batch in train_loader:
+        for batch in data_loader:
             batch_count += 1
             images, labels = batch
             # 1. feed model
@@ -90,27 +92,52 @@ def train(model, optimizer):
                   f'processed={img_checked}/{len(train_dataset)}\t\tloss={loss}')
         torch.save(model.state_dict(), os.path.join(os.getcwd(), f'model_state_dict{5}.pth'))
 
-def test(model, plot=True):
+
+def test(model, data_loader: DataLoader, record=False):
+    record_dir = 'test_records/'
+    records_df = pd.DataFrame({'processed': [], 'missed': [], 'predictions': [], 'true labels': [], 'differences': []})
+    if not os.path.exists(record_dir):
+        os.mkdir(record_dir)
     with torch.no_grad():
         img_count = 0
         model.eval()
-        for batch in test_loader:
+        batch_count = 0
+        for batch in data_loader:
             images, labels = batch
             predictions = model(images)
             predictions = pred_to_binary(predictions)
 
             img_count += len(images)
-            img_missed = np.sum(np.logical_xor(predictions.numpy(), labels.numpy()))
-            print(f'processed={img_count}/{len(test_dataset)}')
-            print(f'predis: {predictions}')
-            print(f'labels: {labels}')
-            print(f'missed:   {img_missed}/{len(predictions)}')
-            if plot:
-                for i in range(len(images)):
-                    plt.imshow(images[i].permute(1, 2, 0))
-                    plt.title(f'label={test_dataset.classes[labels[i]]}\n'
-                              f'predict={test_dataset.classes[predictions[i]]}')
-                    plt.show()
+            misses_vector = np.bitwise_xor(predictions.numpy(), labels.numpy())
+            img_missed = np.sum(misses_vector)
+
+            print(f'processed: {img_count}/{len(test_dataset)}\t'
+                  f'missed: {img_missed}/{len(predictions)}\n'
+                  f'predis: {predictions}\nlabels: {labels}')
+            if record:
+                # build Dataframe records table:
+                records_df.loc[len(records_df.index)] = [img_count, img_missed,
+                                                         predictions.tolist(),
+                                                         labels.tolist(),
+                                                         misses_vector.tolist()]
+                # plotting image grid:
+                dim = int(np.sqrt(BATCH_SIZE))
+                fig = plt.figure(figsize=(10., 10.))
+                fig.suptitle(f'processed: {len(images)}   missed: {img_missed}')
+                grid = ImageGrid(fig, 111,
+                                 nrows_ncols=(dim, dim),
+                                 axes_pad=0.5)
+                idx = 0
+                for ax, im in zip(grid, [img.permute(1, 2, 0) for img in images]):
+                    ax.imshow(im)
+                    ax.set_title(f'true: {test_dataset.classes[labels[idx]]}\n'
+                                f'pred: {test_dataset.classes[predictions[idx]]}')
+
+                    idx += 1
+                #plt.show()
+                plt.savefig(f'{record_dir}test_batch{batch_count}.png')
+            batch_count += 1
+    records_df.to_csv(f'{record_dir}citrus_dis_test_record.csv')
 def pred_to_binary(pred: torch.Tensor):
     dim = len(pred)
     pred = torch.sigmoid(pred).numpy()
@@ -137,20 +164,18 @@ if __name__ == '__main__':
 
     #   Model setup:
     torch.cuda.empty_cache()
-    #model = ImageClassificationModel(num_of_classes=2).to(device)
-    my_resnet = resnet50()
-    model = nn.Sequential(my_resnet, nn.Linear(in_features=1000, out_features=2))
+    my_resnet_model = nn.Sequential(resnet50(), nn.Linear(in_features=1000, out_features=2))
     #   Optimizer and Loss setup:
-    optimizer = optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
+    optimizer = optim.Adam(params=my_resnet_model.parameters(), lr=LEARNING_RATE)
     loss_func = F.cross_entropy
 
     if CONFIG['LOAD_PREV']:
         print('Loading model state-dict.')
         sd_path = os.path.join(os.getcwd(), f'model_state_dict{5}.pth')
-        model.load_state_dict(torch.load(sd_path))
+        my_resnet_model.load_state_dict(torch.load(sd_path))
     if CONFIG['TRAIN']:
         print('Training model.')
-        train(model, optimizer)
+        train(my_resnet_model, optimizer, data_loader=train_loader)
     if CONFIG['TEST']:
         print('Testing model.')
-        test(model)
+        test(my_resnet_model, data_loader=test_loader, record=True)

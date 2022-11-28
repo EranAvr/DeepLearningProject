19675dd0e46c001f08977a06dfd610a7667960b1
Eran Avraham <48490120+EranAvr@users.noreply.github.com>
2022-11-28 10:11:08 +0200

    Update citrus_diseases.py

--- citrus_diseases.py
+++ citrus_diseases.py
@@ -33,41 +33,6 @@ transformer = torchvision.transforms.Compose(
 )
 
 
-#   Possible test model:
-"""
-class ImageClassificationModel(nn.Module):
-    def __init__(self, num_of_classes):
-        super().__init__()
-        self.network = nn.Sequential(
-                nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3,
-                          stride=1, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2),  # output: 64 x 16 x 16
-
-                nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2),  # output: 128 x 8 x 8
-
-                nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.MaxPool2d(2, 2),  # output: 256 x 4 x 4
-
-                nn.Flatten(),
-                nn.Linear(256 * 28 * 28, 1024),
-                nn.ReLU(),
-                nn.Linear(1024, 512),
-                nn.ReLU(),
-                nn.Linear(512, num_of_classes))
-    def forward(self, xb):
-        return self.network(xb)
-"""
-
 
 def train(model, optimizer, data_loader: DataLoader):
     model.train()
